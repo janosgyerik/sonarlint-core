e71f8b18feb560380bddeee47ef3d5f8668cbb0b
Janos Gyerik <janos@kronos>
2017-09-26 22:07:42 +0200

    Add samples and cli poc

--- instalint-cli/src/main/java/io/instalint/cli/Main.java
+++ instalint-cli/src/main/java/io/instalint/cli/Main.java
@@ -19,10 +19,12 @@
  */
 package io.instalint.cli;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import org.sonarsource.sonarlint.core.StandaloneSonarLintEngineImpl;
@@ -30,7 +32,6 @@ import org.sonarsource.sonarlint.core.client.api.common.LogOutput;
 import org.sonarsource.sonarlint.core.client.api.common.ProgressMonitor;
 import org.sonarsource.sonarlint.core.client.api.common.analysis.AnalysisResults;
 import org.sonarsource.sonarlint.core.client.api.common.analysis.ClientInputFile;
-import org.sonarsource.sonarlint.core.client.api.common.analysis.Issue;
 import org.sonarsource.sonarlint.core.client.api.common.analysis.IssueListener;
 import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneAnalysisConfiguration;
 import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneGlobalConfiguration;
@@ -39,13 +40,18 @@ import org.sonarsource.sonarlint.core.client.api.standalone.StandaloneSonarLintE
 public class Main {
   public static void main(String[] args) throws IOException {
     StandaloneGlobalConfiguration globalConfig = StandaloneGlobalConfiguration.builder()
+      .addPlugin(new File("./core/target/plugins/sonar-python-plugin-1.7.0.1195.jar").toURI().toURL())
+      .addPlugin(new File("./core/target/plugins/sonar-javascript-plugin-2.21.1.4786.jar").toURI().toURL())
+      .addPlugin(new File("./core/target/plugins/sonar-php-plugin-2.10.0.2087.jar").toURI().toURL())
       .build();
     StandaloneSonarLintEngine engine = new StandaloneSonarLintEngineImpl(globalConfig);
 
     Path tmp = newTempDir();
     Path baseDir = newDir(tmp.resolve("base"));
     Path workDir = newDir(tmp.resolve("work"));
-    Iterable<ClientInputFile> inputFiles = Collections.emptyList();
+    InputFileFinder inputFileFinder = new InputFileFinder("**/*.{py,js,php}", null, null, Charset.defaultCharset());
+    Iterable<ClientInputFile> inputFiles = inputFileFinder.collect(Paths.get("samples").toAbsolutePath());
+
     Map<String, String> extraProperties = new HashMap<>();
     StandaloneAnalysisConfiguration config = new StandaloneAnalysisConfiguration(baseDir, workDir, inputFiles, extraProperties);
 
@@ -53,9 +59,10 @@ public class Main {
       System.out.println("handle issue");
     };
     LogOutput logOutput = (formattedMessage, level) -> {
-      System.out.println(formattedMessage);
+      System.out.println("log: " + formattedMessage);
     };
     ProgressMonitor monitor = new ProgressMonitor() {};
+
     AnalysisResults results = engine.analyze(config, issueListener, logOutput, monitor);
     System.out.println(results.failedAnalysisFiles());
     System.out.println(results.fileCount());
